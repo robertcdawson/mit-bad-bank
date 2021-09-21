547c224d8060a5cbfe207ad0d0b06f4623629590
Robert Dawson <manatees@gmail.com>
2021-09-21 00:01:02 -0700

    Add homepage content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,8 @@ function Home() {
   return (
     <Card
       txtcolor="black"
-      header="BadBank Landing Module"
       title="Welcome to the bank"
-      text="You can move around using the navigation bar."
+      text="For all your banking needs"
       body={(<img src="images/bank.png" className="img-fluid" alt="Responsive image" />)}
     />
   );
